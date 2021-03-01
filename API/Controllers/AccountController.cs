2e770f677e755fbfceb21e389cd394708a6a81d8
Anindyo <boseanindyo05@gmail.com>
2021-03-01 12:38:23 +0530

    Login API Method Added

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -12,6 +12,13 @@ namespace API.Controllers
     public class AccountController:BaseApiController
     {
         private readonly DataContext _context;
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+        }
+
+
         public AccountController(DataContext context)
         {
             _context = context;
@@ -35,9 +42,24 @@ namespace API.Controllers
             return user;
         }
 
-        private async Task<bool> UserExists(string username)
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
         {
-            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+            var user = await _context.Users
+                                .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+            
+            if(user == null) return Unauthorized("Invalid Username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i =0; i< computedHash.Length; i++){
+                if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+            }
+            return user;
         }
+
+        
     }
 }
